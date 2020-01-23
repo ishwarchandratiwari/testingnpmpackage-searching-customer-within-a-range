e3c78484d12a52f64f100f602f8344df43185f3f
ishwar chandra tiwari <ishwardeveloper@gmail.com>
2020-01-23 05:50:13 +0530

    make build file

---
+++ build/precache-manifest.f8c0fae729a59ef3b9c890672a368a87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1545cf9670de482a04c6fb7d67fb0216",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f59ca9d72d117e63faa3",
+    "url": "/static/css/main.a2c03a17.chunk.css"
+  },
+  {
+    "revision": "22858ff1e64d6cae6683",
+    "url": "/static/js/2.56f9d531.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.56f9d531.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f59ca9d72d117e63faa3",
+    "url": "/static/js/main.0650a5e0.chunk.js"
+  },
+  {
+    "revision": "ed4626bf8bde9d5f7a75",
+    "url": "/static/js/runtime-main.678cf49e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
